5265adbc1a68a06d42177c8c4e4d112d1391a793
Ruben <34238748+RubenBar@users.noreply.github.com>
2021-10-29 08:37:23 -0400

    Update Proc

--- Interface/proc1.sh
+++ Interface/proc1.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
+python interface.py &
 
 X=0
 while [ ${X} -eq 0 ]
 do
-	proc2_state=$(ps -ef | grep "proc2.sh" | grep -v grep | wc -l | xargs)
+	proc2_state=$(ps -ef | grep "proc2.sh" | grep -v grep | wc -l | xargs)	
+	interface_state=$(ps -ef | grep "interface.py" | grep -v grep | wc -l | xargs)
 	if [ ${proc2_state} -eq 0 ]; then
 		echo "Process stop running"
 		/bin/bash proc2.sh &
+		
+	elif [ ${interface_state} -eq 0 ]; then
+		python interface.py &
+		
 	else
 		echo "Process running"
 	fi
--- Interface/proc2.sh
+++ Interface/proc2.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-
 X=0
 while [ ${X} -eq 0 ]
 do
-	proc2_state=$(ps -ef | grep "proc1.sh" | grep -v grep | wc -l | xargs)
+	proc1_state=$(ps -ef | grep "proc1.sh" | grep -v grep | wc -l | xargs)	
+	interface_state=$(ps -ef | grep "interface.py" | grep -v grep | wc -l | xargs)
 	if [ ${proc2_state} -eq 0 ]; then
 		echo "Process stop running"
 		/bin/bash proc1.sh &
+		
+	elif [ ${interface_state} -eq 0 ]; then
+		if [ ${proc1_state} -eq 0 ]; then
+			python interface.py &
+		fi
+		
 	else
 		echo "Process running"
 	fi
