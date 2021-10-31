865b9c0f0cd0e8007e69d0c3c1703b0c78d1eba2
Ruben Barcelo <ruben.bar.ar@gmail.com>
2021-10-31 15:38:10 +0100

    Fix error in proc2.sh

--- Interface/proc2.sh
+++ Interface/proc2.sh
@@ -2,14 +2,14 @@
 X=0
 while [ ${X} -eq 0 ]
 do
-	proc1_state=$(ps -ef | grep "proc1.sh" | grep -v grep | wc -l | xargs)	
+	proc2_state=$(ps -ef | grep "proc1.sh" | grep -v grep | wc -l | xargs)	
 	interface_state=$(ps -ef | grep "interface.py" | grep -v grep | wc -l | xargs)
 	if [ ${proc2_state} -eq 0 ]; then
 		echo "Process stop running"
 		/bin/bash Interface/proc1.sh &
 		
 	elif [ ${interface_state} -eq 0 ]; then
-		if [ ${proc1_state} -eq 0 ]; then
+		if [ ${proc2_state} -eq 0 ]; then
 			python Interface/interface.py &
 		fi
 	fi
