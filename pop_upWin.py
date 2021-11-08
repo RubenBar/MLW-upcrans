f7e0ed1664e002f4fd2762bcc3f4897e8a0f4ac3
juditcab <91687520+juditcab@users.noreply.github.com>
2021-11-08 21:50:00 +0100

    Update pop_upWin.py

--- pop_upWin.py
+++ pop_upWin.py
@@ -15,14 +15,6 @@ win.title("Ransomware Decryptor")
 #img = PhotoImage(file = "error.png")
 img = PhotoImage(file = "alert.png")
 img = img.subsample(2, 2)
-def open_file():
-	path= filedialog.askopenfile(title="Instructions", filetypes=[('all files', '*.*')])
-	file = open(path, 'r')
-	txt=file.read()
-	label.config(text=txt, font=('Courier 13'))
-	file.close()
-	button.config(state=DISABLED)
-	win.geometry("750x450")
 
 #Create Canvas
 canvas1 = Canvas( win, width = 400, height = 400)
@@ -30,10 +22,5 @@ canvas1.pack(fill="both", expand = True)
 canvas1.create_image(50, 50, image = img, anchor="nw")
 
 canvas1. create_text(225, 30, text = "Your files have been encrypted!!!", font='bold 18', fill="red")
-
-win.overrideredirect(True)
-#Bind with the url to open in a new tab
-#btn= Button(win, text="Contact us!",  width=7, height=2, command= open_file)
-#btn.place(x=150, y=320)
 
 win.mainloop()
