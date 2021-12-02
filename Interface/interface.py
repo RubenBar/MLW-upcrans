d4136af6e1c5faf88f0d40b73bfa10a85db76a4e
Ruben <34238748+RubenBar@users.noreply.github.com>
2021-12-02 21:34:37 +0100

    Update interface

--- Interface/interface.py
+++ Interface/interface.py
@@ -1,6 +1,26 @@
-import tkinter as tk
+#Import tkinter 
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+import webbrowser
 
-window = tk.Tk()
-greeting = tk.Label(text="Hello, Tkinter")
-greeting.pack()
-window.mainloop()
+#Create an instance of tkinter
+win=Tk()
+
+#Set the windows geometry
+win.geometry("450x350")
+win.title("Ransomware Decryptor")
+
+#Create an object of tkinter image
+#img = PhotoImage(file = "error.png")
+img = PhotoImage(file = "alert.png")
+img = img.subsample(2, 2)
+
+#Create Canvas
+canvas1 = Canvas( win, width = 400, height = 400)
+canvas1.pack(fill="both", expand = True)
+canvas1.create_image(50, 50, image = img, anchor="nw")
+
+canvas1. create_text(225, 30, text = "Your files have been encrypted!!!", font='bold 18', fill="red")
+
+win.mainloop()
